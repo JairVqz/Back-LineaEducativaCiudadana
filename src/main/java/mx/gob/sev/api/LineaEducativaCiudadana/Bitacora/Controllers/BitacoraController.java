9b6d1848d37e1fd4b1a376faba325d71ea06444e
JairVqz <jair10.vasquez11@gmail.com>
2025-10-03 15:01:12 -0600

    bitacora

--- src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Bitacora/Controllers/BitacoraController.java
+++ src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Bitacora/Controllers/BitacoraController.java
@@ -1,8 +1,12 @@
 package mx.gob.sev.api.LineaEducativaCiudadana.Bitacora.Controllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,14 +24,33 @@ public class BitacoraController {
 
     @GetMapping("/findAll")
     @Transactional(readOnly = true)
-    public List<Bitacora> findAll(){
+    public List<Bitacora> findAll() {
         return this.bitacoraImpl.findAll();
     }
-    
+
+    @GetMapping("/findAllActiveByRange")
+    @Transactional(readOnly = true)
+    public ResponseEntity<List<Object[]>> findAllActiveByRange(
+            @RequestParam String fecha_inicio,
+            @RequestParam String fecha_fin) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDateTime fechaInicio = LocalDate.parse(fecha_inicio, formatter).atStartOfDay(); // 2025-09-30T00:00:00
+        LocalDateTime fechaFin = LocalDate.parse(fecha_fin, formatter).atTime(23, 59, 59); // 2025-10-02T23:59:59
+
+        List<Object[]> resultados = bitacoraImpl.findAllActiveByRange(fechaInicio, fechaFin);
+
+        if (resultados.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(resultados);
+    }
+
     @PostMapping
     @Transactional
-    public Bitacora save(@RequestBody Bitacora bitacora){
+    public Bitacora save(@RequestBody Bitacora bitacora) {
         return this.bitacoraImpl.save(bitacora);
     }
-    
+
 }
--- src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Bitacora/Repositories/BitacoraRepository.java
+++ src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Bitacora/Repositories/BitacoraRepository.java
@@ -1,9 +1,23 @@
 package mx.gob.sev.api.LineaEducativaCiudadana.Bitacora.Repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import mx.gob.sev.api.LineaEducativaCiudadana.Bitacora.Models.Bitacora;
 
-public interface BitacoraRepository extends JpaRepository<Bitacora, Long>{
-    
+public interface BitacoraRepository extends JpaRepository<Bitacora, Long> {
+
+    /*@Query(value = "SELECT * FROM tbl_bitacora WHERE activo=1 AND CAST(fecha AS DATE) BETWEEN :fecha_inicio AND :fecha_fin ORDER BY idBitacora", nativeQuery = true)
+    List<Bitacora> findAllActiveByRange(@Param("fecha_inicio") String fecha_inicio,
+            @Param("fecha_fin") String fecha_fin);*/
+
+    @Query("SELECT  b.idBitacora, b.idEntidad, b.entidad, b.accion, b.usuario.nombre, b.fecha " +
+            "FROM Bitacora b WHERE b.activo = 1 AND b.fecha BETWEEN :fecha_inicio AND :fecha_fin ORDER BY b.idBitacora")
+    List<Object[]> findAllActiveByRange(@Param("fecha_inicio") LocalDateTime fecha_inicio,
+            @Param("fecha_fin") LocalDateTime fecha_fin);
+
 }
