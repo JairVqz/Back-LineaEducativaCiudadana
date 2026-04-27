810d6ab03f104d00631dc6e83b41ebe205985fbd
dansanchez <dansanchez@msev.gob.mx>
2026-04-27 11:16:48 -0600

    hasta aquí todo bien en el reporte

--- src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Reportes/ReportController.java
+++ src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Reportes/ReportController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,14 +20,24 @@ public class ReportController {
     }
 
     @GetMapping("/ejemploReporte")
-    public ResponseEntity<byte[]> generarReporte(){
-        try{
-            byte[] report= reportService.generarReport("ejemploReporte");
-            HttpHeaders headers =  new HttpHeaders();
+    public ResponseEntity<byte[]> generarReporte(
+            @RequestParam String fechaInicio,
+            @RequestParam String fechaFin
+    ) {
+        try {
+            byte[] report = reportService.generarReport(
+                    "reportePeriodo",
+                    fechaInicio,
+                    fechaFin
+            );
+
+            HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_PDF);
             headers.add("Content-Disposition", "inline; filename=ejemploReporte.pdf");
+
             return new ResponseEntity<>(report, headers, HttpStatus.OK);
-        }catch (Exception e){
+
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
--- src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Reportes/ReportService.java
+++ src/main/java/mx/gob/sev/api/LineaEducativaCiudadana/Reportes/ReportService.java
@@ -1,6 +1,7 @@
 package mx.gob.sev.api.LineaEducativaCiudadana.Reportes;
 
 import java.io.InputStream;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,8 +9,6 @@ import javax.sql.DataSource;
 
 import org.springframework.stereotype.Service;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
-import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -17,21 +16,33 @@ import net.sf.jasperreports.engine.JasperPrint;
 @Service
 public class ReportService {
 
-    private final DataSource dataSource; 
+    private final DataSource dataSource;
 
     public ReportService(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public byte [] generarReport(String reportName) throws Exception {
-        //Cargar el reporte
-        InputStream reportStream = this.getClass().getResourceAsStream("/Reportes/" + reportName + ".jasper");
-        Map<String, Object> params = null;
-        //Llenar el reporte
-        JasperPrint jasperPrint = JasperFillManager.fillReport(reportStream, params, dataSource.getConnection());
-        //Exporta a un reporte de tipo PDF
+    public byte[] generarReport(String reportName, String fechaInicio, String fechaFin) throws Exception {
+
+        InputStream reportStream = this.getClass()
+                .getResourceAsStream("/Reportes/" + reportName + ".jasper");
+
+        if (reportStream == null) {
+            throw new RuntimeException("No se encontró el .jasper");
+        }
+
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("fecha_inicio", Date.valueOf(fechaInicio));
+        params.put("fecha_fin", Date.valueOf(fechaFin));
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(
+                reportStream,
+                params,
+                dataSource.getConnection()
+        );
+
         return JasperExportManager.exportReportToPdf(jasperPrint);
-    
     }
 
 }
